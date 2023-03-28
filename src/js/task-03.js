e1ac5073bcd2eb43e7d97237357317f11daad0f0
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-03-28 15:39:41 +0300

    Refactor task-3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -9,14 +9,17 @@
 
 const images = [
   {
+    class1: 'first',
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
+    class1: 'second',
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
+    class1: 'third',
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
@@ -25,9 +28,10 @@ const images = [
 const container = document.querySelector('.gallery');
 
 
-const markup = images.map(({url, alt}) => `<li>
-<img src=${url}
-alt=${alt}>
+const markup = images.map(({class1, url, alt}) =>
+`<li class=${class1}  >
+ <img src=${url}
+ alt=${alt}>
 </li>`).join('');
 
 container.insertAdjacentHTML('beforeend', markup);
