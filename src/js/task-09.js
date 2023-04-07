8622ea47e8c886143881d0227661ea5afc512320
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-04-07 23:02:39 +0300

    Complete task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonColor = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+buttonColor.addEventListener('click', onClick);
+
+function onClick(){
+  body.style.backgroundColor = getRandomHexColor();
+}
